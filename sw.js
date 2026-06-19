7aec619aee7f3d98365f290a0ce1f83c4c082407
mauromendeztuem-byte <mauromendez.tuem@gmail.com>
2026-06-19 01:15:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-const CACHE='tuem-v1';
+const CACHE='tuem-v3';
 const ASSETS=['/','index.html'];
 
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('index.html')))
+    fetch(e.request).then(r=>{
+      const copy=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return r;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('index.html')))
   );
 });
